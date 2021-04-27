7e0a97136ce35ac2d5e44c9bdd14d83e621a6801
aadhinkarthik <aadhinkarthik@gmail.com>
2021-04-28 00:37:03 +0530

    DC: Established MongoDB (Atlas) connection

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// To get global variable for default.json file {config}
+const config = require('config');
+const db = config.get('mongoURL');
+
+const connectDB = async () => {
+
+    try {
+
+        await mongoose.connect(db, {
+            // To avoid deprecated behaviour
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Database: Connected successfully to MongoDB Atlas');
+
+    } catch (err) {
+        console.log(err.message);
+        process.exit(1); // Exit process while failure
+    }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -7,4 +7,7 @@ app.get('/', (req, res) => res.send('API is started & running'));
 // Look for env variable called port, while deploying in Heroku
 const PORT = process.env.PORT || 5555;
 
+const connectDB = require('./config/db');
+connectDB();
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
