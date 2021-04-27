3515be628524a2e13d54f332a0dd6bc460156ee4
aadhinkarthik <aadhinkarthik@gmail.com>
2021-04-28 01:20:10 +0530

    DC: Improved Code alignment

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Establish DB connection
+connectDB();
 
 app.get('/', (req, res) => res.send('API is started & running'));
 
 // Look for env variable called port, while deploying in Heroku
 const PORT = process.env.PORT || 5555;
-
-const connectDB = require('./config/db');
-connectDB();
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
